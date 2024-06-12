b39eb8a8600d90b0e95dc4eb876839eb3e80f2d1
Brock Jenkinson <brockjenkinson@gmail.com>
2024-06-11 23:25:38 -0400

    feat: Refactor code to allow plant search functionality and calculate average height

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # this is the main file that will run the program
 
 # Import the functions from the utility file
-from utility import find_object, compute_average
+from utility import find_plant, compute_average_height
 
 # Define the initial string
 data_string = "Rose,Flower,35; Oak,Tree,180; Tulip,Flower,40; Pine,Tree,200; Daisy,Flower,25;"
 
-# Task 1: Find if there is a plant named tulip in the data string and display its info
-find_plant(data_string, "Tulip")
+# Get user input for the plant search
+search_term = input("Enter the name of the plant you want to find: ")
+
+# Task 1: Find if the plant exists in the data string and display its info
+find_plant(data_string, search_term)
 
 # Task 2: Calculate the average height of all plants
 average_height = compute_average_height(data_string)
